716f46b47daf94e38769e007833919c5545d6454
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2024-02-12 21:38:37 +0000

    Refactoring code

--- src/main/java/com/ilan/h2/config/OpenApiConfig.java
+++ src/main/java/com/ilan/h2/config/OpenApiConfig.java
@@ -8,7 +8,6 @@ import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
