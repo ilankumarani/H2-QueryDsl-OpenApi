1eb5135024a5fad463dc294271a0182f0a6b4fb6
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2024-02-12 16:25:47 +0000

    Refactoring by profile

---
+++ src/main/java/com/ilan/h2/config/OpenApiConfig.java
@@ -0,0 +1,7 @@
+package com.ilan.h2.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+}
---
+++ src/main/java/com/ilan/h2/repository/BlogRepository.java
@@ -0,0 +1,38 @@
+package com.ilan.h2.repository;
+
+
+import com.ilan.h2.entity.Owner;
+import com.ilan.h2.entity.QBlog;
+import com.ilan.h2.entity.QOwner;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class BlogRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Owner> findOwnerByName(String name) {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
+        QOwner qOwner = QOwner.owner;
+        return queryFactory.selectFrom(qOwner)
+                .where(qOwner.name.eq(name))
+                .fetch();
+    }
+
+    public List<Owner> findOwnerByNameAndTitle(String name, String title) {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
+        QOwner qOwner = QOwner.owner;
+        QBlog qBlog = QBlog.blog;
+        return queryFactory.selectFrom(qOwner)
+                .innerJoin(qBlog)
+                .where(qOwner.name.eq(name))
+                .where(qBlog.title.eq(title))
+                .fetch();
+    }
+}
