2a9e2d0ba3f21ae04758d1fcfac6caf7a36e1b8a
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2024-03-28 16:04:44 +0000

    Read me

--- src/main/java/com/ilan/h2/controller/blog/BlogController.java
+++ src/main/java/com/ilan/h2/controller/blog/BlogController.java
@@ -2,6 +2,7 @@ package com.ilan.h2.controller.blog;
 
 import com.ilan.h2.entity.Blog;
 import com.ilan.h2.repository.BlogRepository;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,7 @@ public class BlogController {
     private final BlogRepository blogRepository;
 
     @GetMapping(value = "/findBlogByTitle/{title}")
-    public List<Blog> findBlogByTitle(@PathVariable("title") String title) {
+    public List<Blog> findBlogByTitle(@Parameter(description = "titleName", required = true, example="ALPHA") @PathVariable(value = "title") String title) {
         return blogRepository.findBlogByTitle(title);
     }
 }
