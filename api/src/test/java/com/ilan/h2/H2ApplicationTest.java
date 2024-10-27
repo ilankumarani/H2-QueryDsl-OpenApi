8f8ff2034ff29e5db18d89c3db0b262fc2844391
ilankumaran <kumaranilan@yahoo.com>
2024-10-27 16:52:26 +0000

    Adding test case to API project

---
+++ api/src/test/java/com/ilan/h2/H2ApplicationTest.java
@@ -0,0 +1,25 @@
+package com.ilan.h2;
+
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS
+, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT
+, properties = {"server.port=8090"})
+@RequiredArgsConstructor(onConstructor_ = @__(@Autowired))
+public class H2ApplicationTest {
+
+    private final ApplicationContext applicationContext;
+
+    @Test
+    public void context(){
+        assertNotNull(applicationContext);
+    }
+
+}
