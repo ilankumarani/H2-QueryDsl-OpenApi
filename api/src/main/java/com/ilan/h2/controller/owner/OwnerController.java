4223b787bf02854359b575d88b8cdafdd1a22dd6
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2024-10-28 08:11:15 +0000

    Adding findOwnerByNameAndTitle to Controller

--- api/src/main/java/com/ilan/h2/controller/owner/OwnerController.java
+++ api/src/main/java/com/ilan/h2/controller/owner/OwnerController.java
@@ -2,6 +2,7 @@ package com.ilan.h2.controller.owner;
 
 import com.ilan.h2.entity.Owner;
 import com.ilan.h2.service.OwnerRepositoryService;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,12 @@ public class OwnerController {
     private final OwnerRepositoryService ownerRepository;
 
     @GetMapping(value = "/findOwnerByName/{name}")
-    public List<Owner> findOwnerByName(@PathVariable("name") String name) {
+    public List<Owner> findOwnerByName(@Parameter(description = "Name", required = true, example = "Ilankumaran") @PathVariable("name") String name) {
         return ownerRepository.findOwnerByName(name);
+    }
+
+    @GetMapping(value = "/findOwnerByNameAndTitle/{name}/{title}")
+    public List<Owner> findOwnerByNameAndTitle(@Parameter(description = "Name", required = true, example = "Ilankumaran") @PathVariable("name") String name, @Parameter(description = "titleName", required = true, example = "Snow") @PathVariable(value = "title") String title) {
+        return ownerRepository.findOwnerByNameAndTitle(name, title);
     }
 }
