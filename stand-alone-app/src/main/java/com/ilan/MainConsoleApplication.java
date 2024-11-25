3a2bba5181a2be5ca33e678d2266839c2afa3d1a
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2024-11-25 10:25:42 +0000

    Code refactoring

--- stand-alone-app/src/main/java/com/ilan/MainConsoleApplication.java
+++ stand-alone-app/src/main/java/com/ilan/MainConsoleApplication.java
@@ -1,14 +1,17 @@
 package com.ilan;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class MainConsoleApplication {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(MainConsoleApplication.class);
         app.setBannerMode(Banner.Mode.LOG);
         app.run(args);
+        log.info("########## COMPLETED ##########");
     }
 }
