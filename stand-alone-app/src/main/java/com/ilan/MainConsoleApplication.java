2ff354b3e369f4bd8b8f579f94408c83bcc694fb
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2024-11-25 01:28:22 +0000

    Code refactoring

--- stand-alone-app/src/main/java/com/ilan/MainConsoleApplication.java
+++ stand-alone-app/src/main/java/com/ilan/MainConsoleApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootConsoleApplication {
+public class MainConsoleApplication {
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(SpringBootConsoleApplication.class);
-        app.setBannerMode(Banner.Mode.OFF);
+        SpringApplication app = new SpringApplication(MainConsoleApplication.class);
+        app.setBannerMode(Banner.Mode.LOG);
         app.run(args);
     }
 }
