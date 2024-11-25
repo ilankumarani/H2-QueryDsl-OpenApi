0a11f0cc67cc259a25eb15282b56057ea93f1afc
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2024-11-25 08:19:09 +0000

    Code refactoring

--- stand-alone-app/src/main/java/com/ilan/config/SqlQueryGeneratorConfig.java
+++ stand-alone-app/src/main/java/com/ilan/config/SqlQueryGeneratorConfig.java
@@ -2,6 +2,7 @@ package com.ilan.config;
 
 import com.querydsl.sql.codegen.MetaDataExporter;
 import com.querydsl.sql.codegen.MetadataExporterConfigImpl;
+import lombok.SneakyThrows;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,8 @@ import org.springframework.context.annotation.DependsOn;
 
 import javax.sql.DataSource;
 import java.io.File;
+import java.net.URL;
+import java.net.URLDecoder;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -19,12 +22,10 @@ public class SqlQueryGeneratorConfig {
     @Bean
     public CommandLineRunner sqlQueryDslGenerator(DataSource dataSource) {
         return args -> {
-            ClassLoader classLoader = this.getClass().getClassLoader();
-            System.out.println(classLoader.getDefinedPackages());
             java.sql.Connection conn = dataSource.getConnection();
             MetadataExporterConfigImpl metadataExporterConfig = getMetadataExporterConfig();
             MetaDataExporter exporter = new MetaDataExporter(metadataExporterConfig);
-            exporter.export(conn.getMetaData());
+            //exporter.export(conn.getMetaData());
         };
     }
 
@@ -39,9 +40,18 @@ public class SqlQueryGeneratorConfig {
         return metadataExporterConfig;
     }
 
+    @SneakyThrows
     public static Path getSrcMainPath() {
-        String userDir = System.getProperty("user.dir");
-        Path srcMain = Paths.get(userDir, "api", "src", "main", "java");
+        URL resourceUrl = SqlQueryGeneratorConfig.class.getResource("");
+        Path resourcePath = Paths.get(resourceUrl.toURI());
+        Path absolutePath = resourcePath.toAbsolutePath();
+        String pathWithOutTarget = absolutePath.toString().replaceFirst("target\\.*", "");
+
+        System.out.println("absolutePath >>>>"+absolutePath.toString());
+
+
+        Path srcMain = Paths.get(pathWithOutTarget.toString(), "src", "main", "java");
+        System.out.println(srcMain.toString());
         return srcMain;
     }
 }
