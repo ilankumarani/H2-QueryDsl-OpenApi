f3618afebb5053fe11377958a9d65f8d7520c32f
ilankumaran <kumaranilan@yahoo.com>
2024-12-15 11:41:20 +0000

    Query DSL sql Changes

--- api/src/main/java/com/ilan/h2/repository/BlogQueryDslRepository.java
+++ api/src/main/java/com/ilan/h2/repository/BlogQueryDslRepository.java
@@ -1,15 +1,15 @@
-//package com.ilan.h2.repository;
-//
-//import com.ilan.h2.entity.Blog;
-//import com.ilan.h2.entity.QBlog;
-//import io.github.openfeign.querydsl.jpa.spring.repository.QuerydslJpaRepository;
-//
-//import static com.querydsl.jpa.JPAExpressions.selectFrom;
-//
-//
-//public interface BlogQueryDslRepository extends QuerydslJpaRepository<Blog, Long> {
-//
-//  default Blog findOneBy(String title) {
-//    return selectFrom(QBlog.blog).where(QBlog.blog.title.eq(title)).fetchOne();
-//  }
-//}
+package com.ilan.h2.repository;
+
+import com.ilan.h2.entity.Blog;
+import com.ilan.h2.entity.QBlog;
+import io.github.openfeign.querydsl.jpa.spring.repository.QuerydslJpaRepository;
+
+import static com.querydsl.jpa.JPAExpressions.selectFrom;
+
+
+public interface BlogQueryDslRepository extends QuerydslJpaRepository<Blog, Long> {
+
+  default Blog findOneBy(String title) {
+    return selectFrom(QBlog.blog).where(QBlog.blog.title.eq(title)).fetchOne();
+  }
+}
