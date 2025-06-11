ad3f667afd175b1ae83ba0308a5f5e5a414b3ef6
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2025-06-11 23:59:33 +0100

    refactoring

--- api/src/main/resources/h2/data.sql
+++ api/src/main/resources/h2/data.sql
@@ -1,8 +1,14 @@
-insert into OWNER_SCHEMA.OWNER_DETAILS (id, name, email) VALUES (1, 'ILAN', 'ilankumaran.i@gmail.com');
-insert into OWNER_SCHEMA.OWNER_DETAILS (id, name, email) VALUES (2, 'GANESH', 'kumaranilan@yahoo.com');
+insert into OWNER_SCHEMA.OWNER_DETAILS
+(id, name, email)
+    VALUES
+(1, 'ILAN', 'ilankumaran.i@gmail.com'),
+(2, 'GANESH', 'kumaranilan@yahoo.com');
 
 
-insert into BLOG_SCHEMA.BLOG_DETAILS (id ,category, content, title, owner_id)VALUES (11, 'ANIMAL', 'DOG', 'ALPHA', 1);
-insert into BLOG_SCHEMA.BLOG_DETAILS (id ,category, content, title, owner_id)VALUES (22, 'BIRD', 'PEACOCK', 'BETA', 1);
-insert into BLOG_SCHEMA.BLOG_DETAILS (id ,category, content, title, owner_id)VALUES (33, 'AUTOMOBILE', 'CAR', 'DELTA', 2);
-insert into BLOG_SCHEMA.BLOG_DETAILS (id ,category, content, title, owner_id)VALUES (44, 'VEHICLE', 'BIKE', 'GAMMA', 2);
+insert into BLOG_SCHEMA.BLOG_DETAILS
+(id ,category, content, title, owner_id)
+    VALUES
+(11, 'ANIMAL', 'DOG', 'ALPHA', 1),
+(22, 'BIRD', 'PEACOCK', 'BETA', 1),
+(33, 'AUTOMOBILE', 'CAR', 'DELTA', 2),
+(44, 'VEHICLE', 'BIKE', 'GAMMA', 2);
